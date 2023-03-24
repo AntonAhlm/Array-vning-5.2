32ee95140b61ec3d46f8d94cdbc09a7480d6bd20
Anton <>
2023-03-24 09:16:22 +0100

    Add project files.

---
+++ Array Övning 5.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur många sporter har du testat? : ");
+            int AntalSporter=Convert.ToInt32(Console.ReadLine());
+
+            string[] Sport = new string[AntalSporter];
+
+            for(int i=0;i<AntalSporter;i++)
+            {
+                Console.Write("Sport " + (i + 1) + ": ");
+                Sport[i] = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("Sporterna du har testat:");
+
+            for(int i=0;i<AntalSporter;i++)
+            {
+                Console.WriteLine(Sport[i]);
+            }
+        }
+    }
+}
